ac90185c8136677d231f5b7a4ed8a7eadd5439ce
Dmitriy Martynov <deimon27@gmail.com>
2012-03-09 17:50:50 +0400

    * cleanup code of noise generation

--- include/geoMaker.h
+++ include/geoMaker.h
@@ -2,6 +2,7 @@
 #define __GEOMAKER_H__
 
 #include <region.h>
+#include <perlin.h>
 
 namespace cube
 {
@@ -13,33 +14,9 @@ namespace cube
     static void RenderFilling(cube::Region* rg);
 
   protected:
-    static inline float Noise2D(int x, int y)
-    {
-      int n = x + y * 57;
-      n = (n<<13) ^ n;
-      return ( 1.0f - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) /
-        1073741824.0f);
-    }
+    static Perlin* _perlin3d;
+    static Perlin* _perlin2d;
 
-    static inline float Cosine_Interpolate(float x, float y, float a)
-    {
-      float fac1 = 3*powf(1-a, 2) - 2*powf(1-a,3);
-      float fac2 = 3*powf(a, 2) - 2*powf(a, 3);
-      return x*fac1 + y*fac2;
-    }
-
-    static inline float SmoothedNoise2D(float x, float y)
-    {
-      float corners = ( Noise2D(x-1, y-1)+Noise2D(x+1, y-1)+
-        Noise2D(x-1, y+1)+Noise2D(x+1, y+1) ) / 16;
-      float sides   = ( Noise2D(x-1, y)  +Noise2D(x+1, y)  +
-        Noise2D(x, y-1)  +Noise2D(x, y+1) ) /  8;
-      float center  =  Noise2D(x, y) / 4;
-      return corners + sides + center;
-    }
-
-    static float CompileNoise(float x, float y);
-    static int PerlinNoise_2D(float x,float y,float factor);
     static void GenNoise(cube::Region* rg, float rnd);
   };
 }
--- src/geoMaker.cpp
+++ src/geoMaker.cpp
@@ -1,15 +1,15 @@
 #include <geoMaker.h>
-#include "perlin.h"
 #include <regionManager.h>
 #include <light.h>
 
 using namespace cube;
 
+Perlin* GeoMaker::_perlin3d = new Perlin(2, 1, 1.0f, 123);
+Perlin* GeoMaker::_perlin2d = new Perlin(1, 1, 1.0f, 123);
+
 void GeoMaker::CubFilling(cube::Region* rg, float rnd)
 {
   GenNoise(rg, rnd);
-
-  Perlin* perlin = new Perlin(2, 1, 1.0f, 123);
 
   for(int k = REGION_HEIGHT - 1; k >= 0; k--)
   {
@@ -19,7 +19,7 @@ void GeoMaker::CubFilling(cube::Region* rg, float rnd)
       {
         cube::CubRegion cubReg = rg->GetCub(i, j, k);
         if(cubReg.GetCubType() != cube::Cub::Air && 
-          perlin->Get((float)(i + rg->GetX() * REGION_WIDTH)/30.0f, (float)(j + rg->GetY() * REGION_WIDTH)/30.0f, (float)k/20.0f) < -0.4f)
+          _perlin3d->Get((float)(i + rg->GetX() * REGION_WIDTH)/30.0f, (float)(j + rg->GetY() * REGION_WIDTH)/30.0f, (float)k/20.0f) < -0.4f)
         {
           cubReg.SetCubType(cube::Cub::Air);
 
@@ -136,74 +136,8 @@ void GeoMaker::RenderFilling(cube::Region* rg)
   }
 }
 
-float GeoMaker::CompileNoise(float x, float y)
-{
-  float int_X    = int(x);//цела€ часть х
-  float fractional_X = x - int_X;//дробь от х
-  //аналогично у
-  float int_Y    = int(y);
-  float fractional_Y = y - int_Y;
-  //получаем 4 сглаженных значени€
-  float v1 = SmoothedNoise2D(int_X,     int_Y);
-  float v2 = SmoothedNoise2D(int_X + 1, int_Y);
-  float v3 = SmoothedNoise2D(int_X,     int_Y + 1);
-  float v4 = SmoothedNoise2D(int_X + 1, int_Y + 1);
-  //интерполируем значени€ 1 и 2 пары и производим интерпол€цию между ними
-  float i1 = Cosine_Interpolate(v1 , v2 , fractional_X);
-  float i2 = Cosine_Interpolate(v3 , v4 , fractional_X);
-  //€ использовал косинусною интерпол€цию »ћ’ќ лучше
-  //по параметрам быстрота-//качество
-  return Cosine_Interpolate(i1 , i2 , fractional_Y);
-}
-
-int GeoMaker::PerlinNoise_2D(float x,float y,float factor)
-{
-  x += 32000.0f;
-  y += 32000.0f;
-  float total = 0;
-  // это число может иметь и другие значени€ хоть cosf(sqrtf(2))*3.14f
-  // главное чтобы было красиво и результат вас устраивал
-  float persistence=0.5f;
-
-  // экспериментируйте с этими значени€ми, попробуйте ставить
-  // например sqrtf(3.14f)*0.25f или что-то пот€желее дл€ понимани€ J)
-  float frequency = 0.25f;
-  float amplitude=1;//амплитуда, в пр€мой зависимости от значени€ настойчивости
-
-  // вводим фактор случайности, чтобы облака не были всегда одинаковыми
-  // (ћы ведь помним что ф-ци€ шума когерентна?)
-
-  x+= (factor);
-  y+= (factor);
-
-  // NUM_OCTAVES - переменна€, котора€ обозначает число октав,
-  // чем больше октав, тем лучше получаетс€ шум
-  for(int i=0;i<NUM_OCTAVES;i++)
-  {
-    total += CompileNoise(x*frequency, y*frequency) * amplitude;
-    amplitude *= persistence;
-    frequency*=2;
-  }
-  //здесь можно перевести значени€ цвета   по какой-то формуле
-  //например:
-  //total=sqrt(total);
-  // total=total*total;
-  // total=sqrt(1.0f/float(total));
-  //total=255-total;-и.т.д все зависит от желаемого результата
-  total=fabsf(total);
-  int res=int(total*255.0f);//приводим цвет к значению 0-255Е
-  return res;
-}
-
 void GeoMaker::GenNoise(cube::Region* rg, float rnd)
 {
-  //¬з€то с gameDev http://www.gamedev.ru/articles/?id=30126
-  //srand(time(NULL));
-
-  // случайное число, которое призвано внести
-  // случайность в нашу текстуру
-  //float fac =osg::PI*2*10 + ((float)rand() / RAND_MAX)* (osg::PI*3*10 - osg::PI*2*10);
-
   int xOffset = rg->GetX() * REGION_WIDTH;
   int yOffset = rg->GetY() * REGION_WIDTH;
 
@@ -211,10 +145,10 @@ void GeoMaker::GenNoise(cube::Region* rg, float rnd)
   {
     for(int j = -1; j < REGION_WIDTH + 1; j++)
     {
-      //проходим по всем элементам массива и заполн€ем их значени€ми
-      //pNoise[i*size+j]=PerlinNoise_2D(float(i),float(j),fac);
+      int height = 4
+        + (_perlin2d->Get(float(i + xOffset) / 100.0f, float(j + yOffset) / 100.0f) * 0.5f + 0.5f) * (80)
+        + (_perlin2d->Get(float(i + xOffset) / 20.0f, float(j + yOffset) / 20.0f) * 0.5f + 0.5f) * (25);
 
-      int height = REGION_HEIGHT / 2 + ((PerlinNoise_2D(float(i + xOffset) / 3.0f, float(j + yOffset) / 3.0f, rnd) * (REGION_HEIGHT / 3)) / 255);
       rg->SetHeight(i, j, height);
 
       if(i < 0 || i >= REGION_WIDTH || j < 0 || j >= REGION_WIDTH)
@@ -233,16 +167,6 @@ void GeoMaker::GenNoise(cube::Region* rg, float rnd)
         else
           cubReg.SetCubType(cube::Cub::Ground);
       }
-
-      //float light = 1.0f;
-      //for(int z = height+1; z < REGION_HEIGHT; z++)
-      //{
-      //  cube::Cub& cub = rg->GetCub(i, j, z);
-      //  cub._light = light;
-
-      //  if(light > 0.01)
-      //    light -= 0.1f;
-      //}
     }
   }
 }
